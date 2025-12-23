0489e92677dc6bc0869e602d959bf39a66bb1e5a
Diego Guzman <diegp2001@gmail.com>
2025-12-23 13:51:09 -0600

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Diego Guzman on 23/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
